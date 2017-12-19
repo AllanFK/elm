77082fd0165207df01d7d0610c6f5c43a7c568cb
Allan Mikkelsen Flysta <allan.mikkelsen@fotoknudsen.no>
2017-12-19 12:55:16 +0100

    Added ARC

--- src/View/Portrait.elm
+++ src/View/Portrait.elm
@@ -14,11 +14,31 @@ standardPortrait : Model -> Product -> Html Msg
 standardPortrait model = 
     \product -> 
         div [ class "xsellfrontend-standard-portrait-wrapper" ]
-            [ div [ class "xsellfrontend-standard-portrait-image-container"
-                    , style [("background", "url(" ++ product.previewImageUrl ++ ") center 30% / cover no-repeat" )] 
-                  ] [] 
-            , div [ class "xsellfrontend-standard-portrait-text-container" ] []
-            , button [ onClick (AddToCart model.flags product) ][ text "Add to cart"] 
-            
-            
+            [ div [ class "xsellfrontend-standard-portrait-image-container" 
+                    , style [("background", "url(" ++ product.previewImageUrl ++ ") center 30% / cover no-repeat" )] ]
+                []
+            , div [ class "xsellfrontend-standard-portrait-text-container" ]
+                [ span [ class "xsellfrontend-standard-portrait-text-container-header" ]
+                    [ div [ id "xsellfrontendStandardPortraitTextContainerHeader" ]
+                        [ text "Name" ]
+                    , div [ class "xsellfrontend-standard-portrait-text-container-header-description" ]
+                        [ text "Description" ]
+                    ]
+                , div [ class "xsellfrontend-standard-portrait-subheader" ]
+                    [ span []
+                        [ text "Small price" ]
+                    , span [ class "xsellfrontend-standard-portrait-price xsellfrontend-primary-color" ]
+                        [ span [ id "xsellfrontendStandardPortraitPrice" ]
+                            [ text "Price" ]
+                        ]
+                    ]
+                , text "Discount box"
+                , button [ class "xsellfrontend-standard-portrait-add-to-basket-button button tertiary", attribute "data-tam" "add_to_cart_button", onClick (AddToCart model.flags product) ]
+                    [ span []
+                        [ text "Add to cart" ]
+                    ]
+                , div [ class "xsellfrontend-standard-portrait-bottom-text" ]
+                    [ text "View more" ]
+                ]
             ]
+
